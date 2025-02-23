78bdb0c48031239fe4109bcae84d05e2cb4c04f7
kbranch <kbranch@kbranch.net>
2025-02-22 20:58:36 -0500

    Added a batch file to run the tool

---
+++ compare.bat
@@ -0,0 +1,4 @@
+set baseRandomizer=C:\Users\kbranch\Syncthing\LADXR-kbranch
+set newLogicFolder=C:\Users\kbranch\Syncthing\Magpie\LADXR\logic
+
+py -3 main.py --reference-ladxr-path %baseRandomizer% --new-logic-path %newLogicFolder% --dungeons 0 1 2 3 4 5 6 7 8 --no-overworld
